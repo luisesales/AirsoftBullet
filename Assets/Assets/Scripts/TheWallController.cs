ab9a19f42afccfa34f8ddeb6d2a0ac003f98e56b
luisesales <duduosales@hotmail.com>
2025-10-06 03:05:03 -0300

    adding target spawner and adding minor changes in weapons models

---
+++ Assets/Assets/Scripts/TheWallController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TheWallController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetPrefab;
+    private List<Vector3> spawnerPositions = new List<Vector3>();
+
+    [SerializeField]
+    private float cooldown = 1f;
+    private float spawnRate;
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            spawnerPositions.Add(transform.GetChild(i).position);
+        }
+        spawnRate = cooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cooldown <= 0f)
+        {
+            SpawnTarget();
+            cooldown = spawnRate;
+        }
+        else
+        {
+            cooldown -= Time.deltaTime;
+        }
+
+    }
+    public void SpawnTarget()
+    {
+        int randomIndex = Random.Range(0, spawnerPositions.Count);
+        Instantiate(targetPrefab, spawnerPositions[randomIndex], targetPrefab.transform.rotation);
+    }
+}
